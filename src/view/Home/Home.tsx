dc2088912716ba2c84043fa7879fea6ceea53b2e
jefferson <jeffergit@gmail.com>
2020-11-24 11:39:26 +0800

    env

--- src/view/Home/Home.tsx
+++ src/view/Home/Home.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { sampleGet } from "../../http/sample";
+import axios from "axios";
 
 export default function Home() {
+  useEffect(() => {
+    const http: string | undefined = process.env.REACT_APP_HTTP;
+    axios.get("https://reqres.in/api/users?page=2").then((res) => {
+      console.log(res.data);
+    });
+  }, []);
   return (
     <div>
       <h1>Home</h1>
