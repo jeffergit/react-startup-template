018d335be2c4688ba358f53b27f061b1c6e569b3
jefferson <jeffergit@gmail.com>
2020-11-24 14:26:00 +0800

    axios

--- src/view/Home/Home.tsx
+++ src/view/Home/Home.tsx
@@ -1,14 +1,6 @@
-import React, { useEffect } from "react";
-import { sampleGet } from "../../http/sample";
+import React from "react";
 
 export default function Home() {
-  useEffect(() => {
-    sample();
-  }, []);
-  async function sample() {
-    const data = await sampleGet().then((res) => res);
-    console.log(data);
-  }
   return (
     <div>
       <h1>Home</h1>
